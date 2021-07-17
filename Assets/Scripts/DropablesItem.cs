5d499ca09579815975c8a08c2d9ff8d54f062848
KamuHD <dreebf@gmail.com>
2021-07-17 02:02:08 -0300

    Last version
    Excluded unused folders

--- Assets/Scripts/DropablesItem.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Dropables", menuName = "Dropables")]
-public class DropablesItem : ScriptableObject
-{
-    public List<ItemSO> itensToFind;
-}
